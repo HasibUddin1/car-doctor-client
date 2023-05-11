d92288c90e79fbf648aa45aec5a2ed71de461260
IconicLuck <uhasib959@gmail.com>
2023-05-11 12:38:22 +0600

    prepared the checkout page for storing information to mongodb

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,41 +1,65 @@
+import { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
 
 
 const Checkout = () => {
 
     const service = useLoaderData()
 
-    const { title } = service
+    const {user} = useContext(AuthContext)
+
+    const { _id,title, price } = service
+
+    const handleBooking = event => {
+        event.preventDefault()
+
+        const form = event.target
+        const name = form.name.value
+        const email = form.email.value
+        const date = form.date.value
+
+        const order = {
+            customerName : name,
+            email,
+            date,
+            service: title,
+            service_id: _id,
+            price: price
+        }
+
+        console.log(order)
+    }
 
     return (
         <div>
             <h4>Book Now: {title}</h4>
             <div className="card-body">
-                <form>
+                <form onSubmit={handleBooking}>
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
                         <div className="form-control">
                             <label className="label">
-                                <span className="label-text">First Name</span>
+                                <span className="label-text">Name</span>
                             </label>
-                            <input type="text" placeholder="First Name" className="input input-bordered" />
+                            <input type="text" defaultValue={user?.displayName} name="name" placeholder="Name" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
-                                <span className="label-text">Last Name</span>
+                                <span className="label-text">Date</span>
                             </label>
-                            <input type="text" placeholder="Last Name" className="input input-bordered" />
+                            <input type="date" name="date" placeholder="Last Name" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
-                                <span className="label-text">Your Phone</span>
+                                <span className="label-text">Email</span>
                             </label>
-                            <input type="text" placeholder="Your Phone" className="input input-bordered" />
+                            <input type="text" defaultValue={user?.email} name="email" placeholder="Email" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
-                                <span className="label-text">Your Email</span>
+                                <span className="label-text">Amount</span>
                             </label>
-                            <input type="text" placeholder="Your Email" className="input input-bordered" />
+                            <input type="text" name="amount" defaultValue={price} placeholder="Your Email" className="input input-bordered" readOnly />
                         </div>
                     </div>
                     <div className="form-control mt-6">
