08a5d37c71dd0c1a77d625ecfafd15c6d47725bb
IconicLuck <uhasib959@gmail.com>
2023-05-11 07:21:29 +0600

    added some changes on single service checkout page

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -10,6 +10,39 @@ const Checkout = () => {
     return (
         <div>
             <h4>Book Now: {title}</h4>
+            <div className="card-body">
+                <form>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">First Name</span>
+                            </label>
+                            <input type="text" placeholder="First Name" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Last Name</span>
+                            </label>
+                            <input type="text" placeholder="Last Name" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Your Phone</span>
+                            </label>
+                            <input type="text" placeholder="Your Phone" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Your Email</span>
+                            </label>
+                            <input type="text" placeholder="Your Email" className="input input-bordered" />
+                        </div>
+                    </div>
+                    <div className="form-control mt-6">
+                        <input className="btn bg-orange-600 border-none" type="submit" value="Confirm Order" />
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
