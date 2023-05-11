5753efcc3fa4ce1ecfb58db8226b150d111e35fa
IconicLuck <uhasib959@gmail.com>
2023-05-11 18:35:14 +0600

    implemented bookings route for single user

--- src/pages/Shared/NavigationBar/NavigationBar.jsx
+++ src/pages/Shared/NavigationBar/NavigationBar.jsx
@@ -22,7 +22,10 @@ const NavigationBar = () => {
         <li><Link to='/about'>About</Link></li>
         {
             user ?
-            <li><button onClick={handleLogOut}>Log Out</button></li> :
+            <>
+                <li><Link to='/bookings'>My Bookings</Link></li>
+                <li><button onClick={handleLogOut}>Log Out</button></li>
+            </> :
             <li><Link to='/login'>Login</Link></li>
         }
         <li><Link to='/signUp'>Sign Up</Link></li>
